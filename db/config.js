06c06456f2e04ae9dafd036e016fd8c0c344b379
bsllacerda <bruno.lacerda@finaxis.com.br>
2020-04-21 14:31:56 -0300

    Atualizações

--- db/config.js
+++ db/config.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const DB_CONFIG = process.env.DB_CONFIG || 'postgresql://contacts:contacts@localhost:5432/contacts';
+const DB_CONFIG = process.env.DB_CONFIG || 'postgresql://loan-web:loan-web@localhost:5432/loan-web';
 const { Pool } = require('pg');
 
 const pgconn = new Pool({
