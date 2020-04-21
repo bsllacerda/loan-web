705d57be9495d83b120c85c0312c1c0f94151227
bsllacerda <bruno.lacerda@finaxis.com.br>
2020-04-21 16:18:28 -0300

    Update database host

--- db/config.js
+++ db/config.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const DB_CONFIG = process.env.DB_CONFIG || 'postgresql://loan-db:loan-db@172.30.155.201:5432/loan-db';
+const DB_CONFIG = process.env.DB_CONFIG || 'postgresql://loan-db:loan-db@loan-db:5432/loan-db';
 const { Pool } = require('pg');
 
 const pgconn = new Pool({
